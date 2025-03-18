0c01a294e6cbcc51c23e345b1a695561d9b3a754
Chathula0X <konarashan@gmail.com>
2025-03-19 02:37:18 +0530

    frontend-routing

--- client/Client/src/App.jsx
+++ client/Client/src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import React from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-  
-  
+
+  return (
+    <React.Fragment>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+    </React.Fragment>
+
+  );
 }
 
-export default App
+export default App;
