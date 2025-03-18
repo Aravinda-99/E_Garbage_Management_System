c3dc1842e521b8b793fdf74c6c653bbe11d3b66f
Chathula0X <konarashan@gmail.com>
2025-03-19 02:49:09 +0530

    fix2

--- client/Client/src/App.jsx
+++ client/Client/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import './App.css'
 
+import { Routes, Route } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import React from 'react';
+import './App.css';
 
 function App() {
 
   return (
-    <BrowserRouter>
+    <React.Fragment>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Home" element={<Home />} />
       </Routes>
-    </BrowserRouter>
-  )
+
+    </React.Fragment>
+
+  );
 }
 
 export default App;
