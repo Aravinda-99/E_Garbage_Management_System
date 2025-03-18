54185ea24f7325336b02f3f5388f21f5c9b777e6
Chathula0X <konarashan@gmail.com>
2025-03-19 02:38:56 +0530

    routing

--- client/Client/src/App.jsx
+++ client/Client/src/App.jsx
@@ -1,11 +1,18 @@
+<<<<<<< Updated upstream
 import { Routes, Route } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import React from 'react';
 import './App.css';
+=======
+import { useState } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import './App.css'
+>>>>>>> Stashed changes
 
 function App() {
 
   return (
+<<<<<<< Updated upstream
     <React.Fragment>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -14,6 +21,15 @@ function App() {
     </React.Fragment>
 
   );
+=======
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Home" element={<Home />} />
+    </Routes>
+   </BrowserRouter>
+  )
+>>>>>>> Stashed changes
 }
 
 export default App;
